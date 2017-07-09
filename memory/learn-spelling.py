1a42c275b5579619462a5d315bd6819efea238ec
James <james@zingel.co.nz>
2017-07-09 20:07:38 +1200

    Started the spelling program

---
+++ memory/learn-spelling.py
@@ -0,0 +1,8 @@
+# Setup the settings
+# Initialise the words (500 sample and remove words over 8 letters)
+# Do the loop
+
+# Sigh. You can do it! Work hard and don't spend long. Good Luck.
+
+# Affirmative. Mission received. I repeat, mission received.
+pass
