e23ce9ebf6e2ea5102c371f79695fb1bb6047a7f
James <james@zingel.co.nz>
2017-07-12 10:37:21 +1200

    Added stuff to spelling app

--- memory/learn-spelling.py
+++ memory/learn-spelling.py
@@ -1,24 +1,26 @@
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
 # Code taken and adapted from memory.py
 
 import random
 import os
 import time
+import sys
 
 # Constants
 DEFAULT_SPLITTER = ' '
 MY_DIR = os.path.realpath(os.path.dirname(__file__))
 DICTIONARIES_DIR = os.path.join(MY_DIR, 'dictionaries')
+COMMAND = 'python3 ' + str(os.path.join(MY_DIR, __file__))
 
 CORRECT_RESPONSE = ['correct', 'correct', 'well done', 'thats it', 'your on your way', 'nice', 'good job']
 INCORRECT_RESPONSE = ['wrong', 'incorrect', 'incorrect', 'try again later', 'you will get there', 'oh no', 'wrong']
 
 # Settings
 WORD_LENGTH = 8
+NUMBER_WORDS = 30
 
-# os.system("clear")
-# os.system("say " + {{WORD}})
 
-def respond (correct):
+def respond(correct):
     if correct:
         response = random.choice(CORRECT_RESPONSE)
         os.system('say ' + response)
@@ -26,15 +28,16 @@ def respond (correct):
         response = random.choice(INCORRECT_RESPONSE)
         os.system('say ' + response)
 
-def learn (words):
-    '''Actually run the loops of code'''
+
+def learn(words):
+    # Actually run the loops of code
     incorrect = []
     stats = {
-        'first_correct' : 0,
-        'first_incorrect' : 0,
-        'round' : 1,
-        'total_correct' : 0,
-        'total_incorrect' : 0
+        'first_correct': 0,
+        'first_incorrect': 0,
+        'round': 1,
+        'total_correct': 0,
+        'total_incorrect': 0
     }
     random.shuffle(words)
     for word in words:
@@ -43,7 +46,6 @@ def learn (words):
         os.system('clear')
         while True:
             os.system('say ' + word)
-            print('DEBUG: ', word)
             spell = input('?= ')
 
             if spell.lower() == word:
@@ -59,21 +61,29 @@ def learn (words):
                 # They want to hear the word again
                 continue
 
+            elif spell == '+':
+                # They want to hear the word in a different voice
+                os.system('say -v Daniel ' + word)
+                time.sleep(0.5)
+                os.system('say -v Karen ' + word)
+                continue
+
             else:
                 # They got it wrong
                 incorrect.append(word)
+                incorrect.append(word)  # Append it twice so the user really learns the word
                 respond(False)
                 stats['first_incorrect'] += 1
                 stats['total_incorrect'] += 1
-                print('Incorrect.\nCorrect spelling was',word)
-                halt = input('(Press enter to continue)')
+                print('Incorrect.\nCorrect spelling was', word)
+                input('(Press enter to continue)')
                 time.sleep(0.5)
                 break
     while incorrect.__len__() > 0:
         os.system('clear')
         stats['round'] += 1
-        print('Round', stats['round'], '. Retesting on', incorrect)
-        halt = input('(Enter to resume)')
+        print('Round', stats['round'], '. Lets go!')
+        input('(Enter to resume)')
         random.shuffle(incorrect)
         to_remove = []
         for word in incorrect:
@@ -81,7 +91,6 @@ def learn (words):
             os.system('clear')
             while True:
                 os.system('say ' + word)
-                print('DEBUG: ', word)
                 spell = input('?= ')
 
                 if spell.lower() == word:
@@ -97,12 +106,19 @@ def learn (words):
                     # They want to hear the word again
                     continue
 
+                elif spell == '+':
+                    # They want to hear the word in a different voice
+                    os.system('say -v Daniel ' + word)
+                    time.sleep(0.5)
+                    os.system('say -v Karen ' + word)
+                    continue
+
                 else:
                     # They got it wrong
                     respond(False)
                     stats['total_incorrect'] += 1
                     print('Incorrect.\nCorrect spelling was', word)
-                    halt = input('(Press enter to continue)')
+                    input('(Press enter to continue)')
                     time.sleep(0.5)
                     break
         # Remove all words spelled correct
@@ -117,16 +133,20 @@ def learn (words):
     total_per = round((stats['total_correct'] / (stats['total_incorrect'] + stats['total_correct'])*100), 2)
 
     message = {
-    1 : ('You spelt ' + str((stats['total_incorrect'] + stats['total_correct'])) + ' words over ' + str(stats['round']) + ' rounds.'),
-    2 : ('You spelt ' + str(stats['first_correct']) + ' words right and ' + str(stats['first_incorrect']) + ' words wrong in your first round which gave you a ' + str(first_per) + '% word success rate'),
-    3 : ('In total you spelt ' + str(total_per) + '% of your words right in all your rounds.'),
-    4 : ('Thanks for playing!')
+    1: ('You spelt ' + str((stats['total_incorrect'] + stats['total_correct'])) + ' words over ' + str(stats['round']) +
+        ' rounds.'),
+    2: ('You spelt ' + str(stats['first_correct']) + ' words right and ' + str(stats['first_incorrect']) +
+        ' words wrong in your first round which gave you a ' + str(first_per) + '% word success rate'),
+    3: ('In total you spelt ' + str(total_per) + '% of your words right in all your rounds.'),
+    4: 'Thanks for playing!'
     }
 
     for i in message:
         print(message[i])
-def setup (words):
-    '''Setup the words and use any custom settings'''
+
+
+def setup(words):
+    # Setup the words and use any custom settings
 
     # Take a five hundred words sample if words are over this limit
     if words.__len__() > 500:
@@ -145,15 +165,32 @@ def setup (words):
     for word in to_remove:
         words.remove(word)
 
-    if words.__len__() > 30:
-        # Choose 30 words from this list
-        final_words = random.sample(words, 30)
+    message = 'How many words do you want to be tested on? (Enter for default of ' + str(NUMBER_WORDS) + ' words)\n: '
+    num = input(message)
+    if num == '':
+        num = NUMBER_WORDS
     else:
+        try:
+            num = int(num)
+        except:
+            print('Error: Invalid input')
+            return
+
+    if words.__len__() > num:
+        # Choose 30 words from this list
+        print('More than', num, 'words. Shortening...')
+        final_words = random.sample(words, num)
+    else:
+        print('Whoa! Shortened to 500 word limit!')
         final_words = words
 
+
+
     print('Words to be tested on:\n', final_words)
-    halt = input('Ready? (Enter)')
+    print('Remember! Type "-" to hear the word again or "+" to hear the word in a different voice')
+    input('Ready? (Enter)')
     learn(final_words)
+
 
 def main():
     print('Learn Spelling Words App')
@@ -201,4 +238,6 @@ def main():
         return
 
 if __name__ == '__main__':
+    if sys.version_info[0] < 3:
+        exit("Must be run with Python 3. \nTry using: " + COMMAND)
     main()
